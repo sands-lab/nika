90a448b77d12ab52fcff4cebf4e59c0f62cad0aa
alessandrocornacchia <alessandrocornacchia@users.noreply.github.com>
2026-09-22 11:27:25 +0000

    deploy: alessandrocornacchia/nika-website@8773dfa318366ffb3596d716563a25f521818404

---
+++ community/invite-url.js
@@ -0,0 +1,4 @@
+// Rotate this URL when the Slack shared invite expires (~every 30 days).
+// All public docs and README links should point at /community/, not this URL.
+window.NIKA_SLACK_INVITE_URL =
+  "https://join.slack.com/t/nika-euo2790/shared_invite/zt-4al3rmtpe-AviEWrT59oGHs9Mm2NWsxw";
